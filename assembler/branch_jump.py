06b7744f96c357a41c86eb40aa70d37526dcb22f
d1ecoder <hridaysachdeva2007@gmail.com>
2026-03-08 17:38:54 +0530

    Update branch_jump.py

--- assembler/branch_jump.py
+++ assembler/branch_jump.py
@@ -1,3 +1,5 @@
+# branch_jump.py
+
 # Branch instruction definitions (B-Type)
 BRANCH_TYPE = {
     "beq":  ("000", "1100011"),
@@ -21,13 +23,14 @@ def encode_branch(instr, rs1, rs2, imm, REGISTER_MAP, to_signed_binary):
     rs1_binary = REGISTER_MAP[rs1]
     rs2_binary = REGISTER_MAP[rs2]
 
+    # Branch immediate is 13 bits
     imm_binary = to_signed_binary(imm, 13)
 
-    # Split immediate according to B-type format
-    imm_12   = imm_binary[0]       # bit 12
-    imm_10_5 = imm_binary[2:8]     # bits 10-5
-    imm_4_1  = imm_binary[8:12]    # bits 4-1
-    imm_11   = imm_binary[1]       # bit 11
+    # Extract fields according to RISC-V B-type format
+    imm_12   = imm_binary[0]
+    imm_10_5 = imm_binary[2:8]
+    imm_4_1  = imm_binary[8:12]
+    imm_11   = imm_binary[1]
 
     binary = (
         imm_12 +
@@ -49,9 +52,10 @@ def encode_jal(rd, imm, REGISTER_MAP, to_signed_binary):
 
     rd_binary = REGISTER_MAP[rd]
 
+    # JAL immediate is 21 bits
     imm_binary = to_signed_binary(imm, 21)
 
-    # Split immediate according to J-type format
+    # Extract fields according to RISC-V J-type format
     imm_20    = imm_binary[0]
     imm_10_1  = imm_binary[10:20]
     imm_11    = imm_binary[9]
